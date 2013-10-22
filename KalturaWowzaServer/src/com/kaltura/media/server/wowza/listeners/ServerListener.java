632932a72955d569ad92c6f9e01cba3db5fa4205
tan-tan-kanarek <jonathan.kanarek@kaltura.com>
2013-10-22 15:13:42 +0300

    Init Kaltura managers

---
+++ KalturaWowzaServer/src/com/kaltura/media/server/wowza/listeners/ServerListener.java
@@ -0,0 +1,37 @@
+package com.kaltura.media.server.wowza.listeners;
+
+import com.kaltura.media.server.KalturaServer;
+import com.kaltura.media.server.KalturaServerException;
+import com.wowza.wms.application.WMSProperties;
+import com.wowza.wms.logging.WMSLogger;
+import com.wowza.wms.logging.WMSLoggerFactory;
+import com.wowza.wms.server.*;
+
+public class ServerListener implements IServerNotify2 {
+
+	@SuppressWarnings("unchecked")
+	public void onServerConfigLoaded(IServer server) {
+		
+		WMSLogger logger = WMSLoggerFactory.getLogger(null);
+		WMSProperties config = server.getProperties();
+		try {
+			KalturaServer.init(logger, config);
+			logger.info("Initialized Kaltura server");
+		} catch (KalturaServerException e) {
+			logger.error("Failed to initialize Kaltura server: " + e.getMessage());
+		}
+	}
+
+	public void onServerCreate(IServer server) {
+	}
+
+	public void onServerInit(IServer server) {
+	}
+
+	public void onServerShutdownStart(IServer server) {
+	}
+
+	public void onServerShutdownComplete(IServer server) {
+	}
+
+}
