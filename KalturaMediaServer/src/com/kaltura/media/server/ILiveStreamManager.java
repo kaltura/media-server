56c1314604a6f3db050dda4446f1e9739bb914ee
tan-tan-kanarek <jonathan.kanarek@kaltura.com>
2013-10-29 08:44:54 +0200

    Pass the entire entry object to the methods

--- KalturaMediaServer/src/com/kaltura/media/server/ILiveStreamManager.java
+++ KalturaMediaServer/src/com/kaltura/media/server/ILiveStreamManager.java
@@ -1,13 +1,15 @@
 package com.kaltura.media.server;
 
+import com.kaltura.client.KalturaApiException;
 import com.kaltura.client.types.KalturaLiveStreamEntry;
 
 public interface ILiveStreamManager extends IManager {
 
-	public void onPublish(String entryId, int serverIndex);
+	public void onPublish(KalturaLiveStreamEntry liveStreamEntry, int serverIndex);
 	
-	public void onUnPublish(String entryId, int serverIndex);
+	public void onUnPublish(KalturaLiveStreamEntry liveStreamEntry);
 
-	public KalturaLiveStreamEntry get(int partnerId, String entryId);
-
+	public KalturaLiveStreamEntry get(int partnerId, String entryId) throws KalturaApiException;
+	
+	public Integer getDvrWindow(KalturaLiveStreamEntry liveStreamEntry);
 }
