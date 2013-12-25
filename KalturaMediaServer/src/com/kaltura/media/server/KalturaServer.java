452807d06ed0faa70eba2208230eae031087af86
tan-tan-kanarek <Jonathan.Kanarek@kaltura.com>
2013-12-25 10:33:21 +0200

    Added version number to the log

--- KalturaMediaServer/src/com/kaltura/media/server/KalturaServer.java
+++ KalturaMediaServer/src/com/kaltura/media/server/KalturaServer.java
@@ -1,5 +1,9 @@
 package com.kaltura.media.server;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
@@ -12,7 +16,6 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 import org.apache.log4j.Logger;
-
 
 import com.kaltura.client.KalturaClient;
 import com.kaltura.client.KalturaConfiguration;
@@ -41,7 +44,17 @@ public class KalturaServer {
 	protected List<IManager> managers;
 
 	protected KalturaServer() throws KalturaServerException {
-		logger.debug("Initializing Kaltura server");
+		InputStream versionInputStream = getClass().getResourceAsStream("VERSION.txt");
+		BufferedReader versionBufferedReader = new BufferedReader(new InputStreamReader(versionInputStream));
+		String version = "Version not found";
+		
+		try {
+			version = versionBufferedReader.readLine();
+		} catch (IOException e1) {
+			logger.error("Failed to read version file");
+		}
+		
+		logger.debug("Initializing Kaltura server [" + version + "]");
 		KalturaServer.instance = this;
 		
 		try {
