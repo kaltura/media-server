ffc8abcd0520bdfcbccb66024f60ee06b6ed4a57
tan-tan-kanarek <Jonathan.Kanarek@kaltura.com>
2014-01-05 11:19:56 +0200

    Run the demo asynchronously

--- KalturaWowzaServer/src/com/kaltura/media/server/wowza/demo/LiveChannelDemo.java
+++ KalturaWowzaServer/src/com/kaltura/media/server/wowza/demo/LiveChannelDemo.java
@@ -3,7 +3,7 @@ package com.kaltura.media.server.wowza.demo;
 import com.kaltura.media.server.KalturaManagerException;
 import com.kaltura.media.server.wowza.LiveChannelManager;
 
-public class LiveChannelDemo extends LiveChannelManager {
+public class LiveChannelDemo extends LiveChannelManager implements Runnable {
 
 	protected final static String KALTURA_CHANNELS_DEMO_PARTNER_ID = "KalturaChannelsDemoPartnerId";
 	protected final static String KALTURA_CHANNELS_DEMO_CHANNEL_IDS = "KalturaChannelsDemoChannelIds";
@@ -14,6 +14,12 @@ public class LiveChannelDemo extends LiveChannelManager {
 	public void init() throws KalturaManagerException {
 		super.init();
 		
+		new Thread(this).start();
+	}
+
+
+	@Override
+	public void run() {
 		if (serverConfiguration.containsKey(LiveChannelDemo.KALTURA_CHANNELS_DEMO_PARTNER_ID) && serverConfiguration.containsKey(LiveChannelDemo.KALTURA_CHANNELS_DEMO_CHANNEL_IDS)) {
 			
 			partnerId = Integer.parseInt((String) serverConfiguration.get(LiveChannelDemo.KALTURA_CHANNELS_DEMO_PARTNER_ID));
