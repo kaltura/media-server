9ba593a2d0a99d7c142d7ceae4dadd1e5532c9bb
tan-tan-kanarek <Jonathan.Kanarek@kaltura.com>
2014-01-05 11:20:59 +0200

    No need to stop KalturaServer if it's not initialized

--- KalturaWowzaServer/src/com/kaltura/media/server/wowza/listeners/ServerListener.java
+++ KalturaWowzaServer/src/com/kaltura/media/server/wowza/listeners/ServerListener.java
@@ -23,14 +23,15 @@ public class ServerListener implements IServerNotify2 {
 		WMSProperties config = server.getProperties();
 		try {
 			kalturaServer = KalturaServer.init(logger, config);
-			logger.info("Initialized Kaltura server");
+			logger.info("ServerListener::onServerInit Initialized Kaltura server");
 		} catch (KalturaServerException e) {
-			logger.error("Failed to initialize Kaltura server: " + e.getMessage());
+			logger.error("ServerListener::onServerInit Failed to initialize Kaltura server: " + e.getMessage());
 		}
 	}
 
 	public void onServerShutdownStart(IServer server) {
-		kalturaServer.stop();
+		if(kalturaServer != null)
+			kalturaServer.stop();
 	}
 
 	public void onServerShutdownComplete(IServer server) {
