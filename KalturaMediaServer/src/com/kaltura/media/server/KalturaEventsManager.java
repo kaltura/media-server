686be1b9b11cc7fe31c267facffcde5a798a81a6
hila.karimov <hila.karimov@kaltura.com>
2014-02-26 19:43:38 +0200

    Fixes after testing

--- KalturaMediaServer/src/com/kaltura/media/server/KalturaEventsManager.java
+++ KalturaMediaServer/src/com/kaltura/media/server/KalturaEventsManager.java
@@ -6,6 +6,9 @@ import java.util.Map;
 import java.util.Set;
 import java.util.Timer;
 import java.util.TimerTask;
+
+import org.apache.log4j.Logger;
+
 import com.kaltura.media.server.events.IKalturaEvent;
 import com.kaltura.media.server.events.IKalturaEventConsumer;
 import com.kaltura.media.server.events.IKalturaEventType;
@@ -13,9 +16,11 @@ import com.kaltura.media.server.events.IKalturaEventType;
 
 public class KalturaEventsManager{
 
+	protected static Logger logger = Logger.getLogger(KalturaEventsManager.class);
 	static Map<IKalturaEventType, Set<IKalturaEventConsumer>> map = new HashMap<IKalturaEventType, Set<IKalturaEventConsumer>>();
 	
 	public static void registerEventConsumer(IKalturaEventConsumer eventConsumer, IKalturaEventType ... eventTypes){
+		logger.info("Attempting to register event consumer [" + eventConsumer.toString() + "] for event types: [" + eventTypes.toString() + "]");
 		Set<IKalturaEventConsumer> consumersMap = null;
 		
 		for(IKalturaEventType eventType : eventTypes){
@@ -28,24 +33,28 @@ public class KalturaEventsManager{
 			}
 			consumersMap.add(eventConsumer);
 		}
+		
 	}
 
 	public static void raiseEvent(final IKalturaEvent event){
-		
-		TimerTask timerTask = new TimerTask() {
+		logger.info("Raising event of type [" + event.getType() + "]");
+		Set<IKalturaEventConsumer> consumersMap = map.get(event.getType());
+		if (consumersMap == null)
+			return;
 			
-			@Override
-			public void run() {
-				if(map.containsKey(event.getType())){
-					Set<IKalturaEventConsumer> consumersMap = map.get(event.getType());
-					for(IKalturaEventConsumer eventConsumer : consumersMap){
+		for(final IKalturaEventConsumer eventConsumer : consumersMap) {
+			TimerTask timerTask = new TimerTask() {
+				
+				@Override
+				public void run() {
+					if(map.containsKey(event.getType())){
 						eventConsumer.onEvent(event);
 					}
 				}
-			}
-		};
-		
-		Timer timer = new Timer();
-		timer.schedule(timerTask, 0);
+			};
+			
+			Timer timer = new Timer();
+			timer.schedule(timerTask, 0);
+		}
 	}
 }
