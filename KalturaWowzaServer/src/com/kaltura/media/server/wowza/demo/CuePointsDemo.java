2561e9d667b66ed97f4f5e4f0c5f7c44c14aeb33
tan-tan-kanarek <Jonathan.Kanarek@kaltura.com>
2014-03-04 13:02:15 +0200

    Periodic cue-point demo

---
+++ KalturaWowzaServer/src/com/kaltura/media/server/wowza/demo/CuePointsDemo.java
@@ -0,0 +1,56 @@
+package com.kaltura.media.server.wowza.demo;
+
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import org.apache.log4j.Logger;
+
+import com.kaltura.infra.StringUtils;
+import com.kaltura.media.server.managers.KalturaCuePointsManager;
+import com.kaltura.media.server.managers.KalturaManagerException;
+import com.kaltura.media.server.wowza.CuePointsManager;
+import com.wowza.wms.application.IApplicationInstance;
+
+public class CuePointsDemo extends CuePointsManager {
+
+	protected static Logger logger = Logger.getLogger(KalturaCuePointsManager.class);
+	
+	private Timer timer;
+
+	@Override
+	protected void onAppStart(final IApplicationInstance applicationInstance) {
+
+		final Date start = new Date();
+
+		TimerTask timerTask = new TimerTask() {
+			
+			@Override
+			public void run() {
+
+				logger.info("Running");
+				
+				Date now = new Date();
+				float offset = now.getTime() - start.getTime();
+				String id = StringUtils.getUniqueId();
+					
+				synchronized (streams) {
+					for(String entryId : streams.keySet()){
+						try {
+							sendSyncPoint(entryId, id, offset);
+						} catch (KalturaManagerException e) {
+							logger.error(e.getMessage());
+						}
+					}
+				}
+			}
+		};
+		
+		timer = new Timer();
+		timer.schedule(timerTask, 3000, 3000);
+		
+		logger.info("Started");
+
+		super.onAppStart(applicationInstance);
+	}
+}
