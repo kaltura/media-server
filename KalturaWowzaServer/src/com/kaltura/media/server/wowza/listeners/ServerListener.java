c4d1571cad8f08308063f278a20144d6dd5d407f
tan-tan-kanarek <Jonathan.Kanarek@kaltura.com>
2014-07-22 16:13:08 +0300

    Preload kLive

--- KalturaWowzaServer/src/com/kaltura/media/server/wowza/listeners/ServerListener.java
+++ KalturaWowzaServer/src/com/kaltura/media/server/wowza/listeners/ServerListener.java
@@ -4,8 +4,13 @@ import org.apache.log4j.Logger;
 
 import com.kaltura.media.server.KalturaServer;
 import com.kaltura.media.server.KalturaServerException;
+import com.wowza.wms.application.IApplicationInstance;
 import com.wowza.wms.application.WMSProperties;
+import com.wowza.wms.logging.WMSLoggerFactory;
 import com.wowza.wms.server.*;
+import com.wowza.wms.vhost.IVHost;
+import com.wowza.wms.vhost.VHost;
+import com.wowza.wms.vhost.VHostSingleton;
 
 public class ServerListener implements IServerNotify2 {
 
@@ -28,6 +33,8 @@ public class ServerListener implements IServerNotify2 {
 		} catch (KalturaServerException e) {
 			logger.error("ServerListener::onServerInit Failed to initialize Kaltura server: " + e.getMessage());
 		}
+		
+		loadAndLockAppInstance(IVHost.VHOST_DEFAULT, "kLive", IApplicationInstance.DEFAULT_APPINSTANCE_NAME);
 	}
 
 	public void onServerShutdownStart(IServer server) {
@@ -38,4 +45,19 @@ public class ServerListener implements IServerNotify2 {
 	public void onServerShutdownComplete(IServer server) {
 	}
 
+	private void loadAndLockAppInstance(String vhostName, String appName, String appInstanceName)
+	{
+		IVHost vhost = VHostSingleton.getInstance(vhostName);
+		if(vhost != null)
+		{
+			if (vhost.startApplicationInstance(appName, appInstanceName))
+			{
+				vhost.getApplication(appName).getAppInstance(appInstanceName).setApplicationTimeout(0);
+			}
+			else
+			{
+				logger.warn("Application folder ([install-location]/applications/" + appName + ") is missing");
+			}
+		}
+	}
 }
