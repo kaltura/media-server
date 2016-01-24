f2f7ec1f040ab211d316b6613f7b52894e30be12
Gad Yaari <gad.yaari@kaltura.com>
2016-01-24 11:13:08 +0200

    PLAT-3277 Wowza version update to 4.3.0 revision

--- KalturaWowzaServer/src/main/java/com/kaltura/media/server/wowza/packetizer/LiveStreamPacketizerKaltura.java
+++ KalturaWowzaServer/src/main/java/com/kaltura/media/server/wowza/packetizer/LiveStreamPacketizerKaltura.java
@@ -52,21 +52,18 @@ public class LiveStreamPacketizerKaltura extends LiveStreamPacketizerCupertino {
 	}
 
 	@Override
-	// TODO: checkout the tempMap necessity and how to use it correctly according the Wowza's documentation
 	public void startChunkVideoTS(int i1, boolean flag, int j1, int k1, int l1, long l2, int i2, String s1, byte abyte0[], byte abyte1[], String s2, String s3, String s4, String s5, Map<String, Map<String, String>> tempMap) {
 		i1 = calcChunkId(i1);
 		super.startChunkVideoTS(i1, flag, j1, k1, l1, l2, i2, s1, abyte0, abyte1, s2, s3, s4, s5, tempMap);
 	}
 
 	@Override
-	// TODO: checkout the tempMap necessity and how to use it correctly according the Wowza's documentation
 	public void startChunkAudioTS(int i1, boolean flag, int j1, int k1, int l1, long l2, int i2, String s1, byte abyte0[], byte abyte1[], String s2, String s3, String s4, String s5, Map<String, Map<String, String>> tempMap) {
 		i1 = calcChunkId(i1);
 		super.startChunkAudioTS(i1, flag, j1, k1, l1, l2, i2, s1, abyte0, abyte1, s2, s3, s4, s5, tempMap);
 	}
 
 	@Override
-	// TODO: checkout the tempMap necessity and how to use it correctly according the Wowza's documentation
 	public void startChunkTS(int i1, boolean flag, int j1, int k1, int l1, long l2, int i2, String s1, byte abyte0[], byte abyte1[], String s2, String s3, String s4, String s5, Map<String, Map<String, String>> tempMap) {
 		i1 = calcChunkId(i1);
 		super.startChunkTS(i1, flag, j1, k1, l1, l2, i2, s1, abyte0, abyte1, s2, s3, s4, s5, tempMap);
