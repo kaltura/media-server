fd1d19d116a5d565afd308078f3dcaf03553c39a
kalturaguy <guy.jacubovski@kaltura.com>
2018-01-15 09:58:42 +0200

    some fixes

---
+++ installation/kalturaEntryPoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+mkdir /usr/local/WowzaStreamingEngine/applications/kLive
+
+cd /usr/local/WowzaStreamingEngine/conf
+
+# replace config
+sed -e "s#@KALTURA_SERVICE_URL@#$SERVICE_URL#g" -e "s#@KALTURA_PARTNER_ID@#$PARTNER_ID#g" -e "s#@KALTURA_PARTNER_ADMIN_SECRET@#$PARTNER_ADMIN_SECRET#g" Server.xml.template > Server.xml
+sed -e "s#@KALTURA_SERVICE_URL@#$SERVICE_URL#g" -e "s#@KALTURA_PARTNER_ID@#$PARTNER_ID#g" -e "s#@KALTURA_PARTNER_ADMIN_SECRET@#$PARTNER_ADMIN_SECRET#g" ./kLive/Application.xml.template > ./kLive/Application.xml
+
+# set log folder
+mv log4j.properties log4j.properties.template
+sed -e "s#/var/log/#$WOWZA_LOG_DIR/#g" log4j.properties.template > log4j.properties
+
+
+bash /sbin/entrypoint.sh
