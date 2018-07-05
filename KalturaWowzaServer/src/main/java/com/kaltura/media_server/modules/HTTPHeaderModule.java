ac26683318802b06a4c96bddac23b852127d3dc9
david-winder-kaltura <david.winder@kaltura.com>
2018-07-05 16:49:54 +0300

    Check for hostName and try to get if not exist

--- KalturaWowzaServer/src/main/java/com/kaltura/media_server/modules/HTTPHeaderModule.java
+++ KalturaWowzaServer/src/main/java/com/kaltura/media_server/modules/HTTPHeaderModule.java
@@ -13,22 +13,27 @@ public class HTTPHeaderModule extends HTTProvider2Base {
 
     private static final Logger logger = Logger.getLogger(HTTPHeaderModule.class);
 
-    private static String mediaServerhostname;
+    private static String mediaServerhostname = "";
 
     @Override
     public void init() {
         super.init();
+        trySetHostName();
+    }
+
+    private void trySetHostName() {
         try {
             mediaServerhostname = Utils.getMediaServerHostname(false);
         }
         catch (Exception e) {
             logger.warn("Could not get hostname ", e);
-            mediaServerhostname = "default_hostname";
         }
     }
 
     public void onHTTPRequest(IVHost vhost, IHTTPRequest req, IHTTPResponse resp)
     {
+        if (mediaServerhostname.length() == 0)
+            trySetHostName();
         resp.setHeader("X-Me", mediaServerhostname);
         resp.removeHeader("Server");
     }
